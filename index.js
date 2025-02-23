a10501369954958cdb8c7e059be468e9688abcec
Alexknight829 <Alexknight829@gmail.com>
2025-02-24 11:21:17 +1300

    Math

--- index.js
+++ index.js
@@ -2,4 +2,25 @@
 // Learning JS in SDV503 class
 /** multi - line text
  */
-console.log(2+1)
+//console.log(2+1)
+
+//ES5
+var a = 2
+
+//ES6
+const pi = 3.14
+let c = 4
+
+//+,-,/ and *
+
+console.log(5-2)
+
+let num1 = 5
+let num2 = 10
+const result = num1 + num2
+console.log("result= ",result)
+
+let num3 =10
+let num4 = 5
+const result1 = num3 / num4
+console.log("result= ",result1)
